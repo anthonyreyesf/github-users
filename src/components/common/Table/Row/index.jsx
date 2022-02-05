9fa283aaa84d21015014288d3ed0aba1ffe7b1dd
Anthony Reyes <anthonyreyesf@gmail.com>
2022-02-05 14:13:22 -0500

    refactored scope

--- src/components/common/Table/Row/index.jsx
+++ src/components/common/Table/Row/index.jsx
@@ -3,31 +3,12 @@ import React, { useEffect, useState } from "react";
 import * as S from "./styles";
 
 export default function Row(props) {
-  const [followersCount, setFollowersCount] = useState();
-
-  useEffect(() => {
-    const followersDataUrl = props.followers;
-    fetch(`${followersDataUrl}+per_page:100`)
-      .then((res) => res.json())
-      .then((data) => setFollowersCount(data.length));
-  }, []);
-
-  function renderFollowers() {
-    if (followersCount > 100) {
-      return `+${followersCount} followers`;
-    } else if (followersCount >= 0) {
-      return `${followersCount} followers`;
-    } else {
-      return "Followers count not available at this time"
-    }
-  }
-
   return (
     <S.Row>
       <S.Image src={props.imgUrl} alt="" />
       <S.RowDetails>
         <p>{props.name}</p>
-        <p>{renderFollowers()}</p>
+        <a href={props.githubUrl}>{props.githubUrl.slice(8)}</a>
       </S.RowDetails>
     </S.Row>
   );
--- src/components/common/Table/index.jsx
+++ src/components/common/Table/index.jsx
@@ -5,7 +5,7 @@ export default function Table(props) {
   return (
     <div>
       {props.data && props.data.map(item => (
-        <Row imgUrl={item.avatar_url} name={item.login} followers={item.followers_url}/>
+        <Row imgUrl={item.avatar_url} name={item.login} githubUrl={item.url}/>
       ))}
     </div>
   );
